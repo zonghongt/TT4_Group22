aac6ecd92b9be6518db9f4ba26e3d57fa1f1357e
jesslynchang <43201116+jesslynchang@users.noreply.github.com>
2021-05-07 13:46:06 +0800

    API Call for Balance.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {Balance} from './components/Balance';
-// import {Transfer} from './components/Transfer';
 import axios from 'axios';
 
 //how to call API 
 
 function App() {
   return (
-    <div className="container">
+    <div className="App">
         <Balance/>
     </div>
   );

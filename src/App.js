5f755a68c5cc36f64086e3c1d61e2df39f1f9ae9
jesslynchang <43201116+jesslynchang@users.noreply.github.com>
2021-05-07 13:26:47 +0800

    push buggy code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {Balance} from './components/Balance';
+// import {Transfer} from './components/Transfer';
 import axios from 'axios';
 
 //how to call API 
--- src/components/Transfer.js
+++ src/components/Transfer.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import React, {useState, useContext} from 'react'
-import { GlobalContext } from '../context/GlobalState';
+import React, {useState} from 'react'
+
+// import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
   
-    const { addTransaction } = useContext(GlobalContext);
+    // const { addTransaction } = useContext(GlobalContext);
   
-    const onSubmit = e => {
-      e.preventDefault();
+    // const onSubmit = e => {
+    //   e.preventDefault();
   
-      const newTransaction = {
-        id: Math.floor(Math.random() * 100000000),
-        text,
-        amount: +amount
-      }
+    //   const newTransaction = {
+    //     id: Math.floor(Math.random() * 100000000),
+    //     text,
+    //     amount: +amount
+    //   }
   
-      addTransaction(newTransaction);
-    }
+    //   addTransaction(newTransaction);
+    // }
   
     return (
       <>
